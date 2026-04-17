006579d873cad9b515ae72abca7cb485105ce1c2
Bukky <ruky2sure@yahoo.com>
2026-04-16 21:36:57 -0800

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { FaFacebook, FaInstagram, FaTwitter, FaWhatsapp } from "react-icons/fa";
+
 
 const Footer = () => {
   return (
